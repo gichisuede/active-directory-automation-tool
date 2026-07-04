39d307871e28f3873c2b7a525a63453f6a97e471
gichisuede <83381435+gichisuede@users.noreply.github.com>
2026-07-04 19:35:42 +0900

    Add core AD Management functions

---
+++ AD-ManagementTool.ps1
@@ -0,0 +1,145 @@
+<#
+.SYNOPSIS
+    Administrator tool form user management.
+
+.DESCRIPTION
+    This tool can be used to do the following:
+	-Create a User
+	-Disable a User
+	-Unlock User
+	-Add User to Group
+	-Generate Report
+
+.NOTES
+    Author: Tony
+    Project: AD User Management Tool
+#>
+
+# -----------------------------
+# Configuration
+# -----------------------------
+
+$OU = 'OU=remote users,DC=corp,DC=contoso,DC=com'
+
+
+# -----------------------------
+# Functions
+# -----------------------------
+
+function Create-User {
+	$UserToCreate = Read-Host "Enter username to create"
+	$SecurePassword = Read-Host "Please enter password" -AsSecureString
+	New-ADUser -Name $UserToCreate -SamAccountName $UserToCreate -AccountPassword $SecurePassword -Path $OU -Enabled $true
+	
+	$Result = [PSCustomObject]@{
+		Time   = Get-Date
+		Action = 'Create User'
+		Target = $UserToCreate
+		Status = 'Completed'
+    }
+    return $Result
+}
+
+function Disable-User{
+	$UserToDisable = Read-Host "Enter username to disable"
+	Set-ADUser -Identity $UserToDisable -Enabled $False
+
+	$Result = [PSCustomObject]@{
+		Time	=  Get-Date
+		Action	=  'Disable User'
+		Target 	=  $UserToDisable
+		Status	=  'Completed'	
+	}
+	return $Result
+}
+
+function Unlock-User{
+	$UserToUnlock = Read-Host "Enter username to unlock"
+	Unlock-ADAccount -Identity $UserToUnlock 
+	
+	$Result = [PSCustomObject]@{
+		Time	=  Get-Date
+		Action	=  'Unlock User'
+		Target	=  $UserToUnlock
+		Status	=  'Completed'
+	}
+	return $Result
+}
+
+function AddUserTo-Group {
+	$UserToAdd = Read-Host "Please enter user to add to group"
+	$Group = Read-Host "Please enter target group"
+	Add-ADGroupMember -Identity $Group -Members $UserToAdd
+
+	$Result = [PSCustomObject]@{
+		Time	=  Get-Date
+		Action	=  'Add User to Group'
+		Target	=  $UserToAdd
+		Group	=  $Group
+		Status	=  'Completed'
+	}
+	return $Result
+}
+
+function Generate-Report{
+		$ReportList | Export-Csv -Path 'C:\project\destinationfolder\csv\UserManagementReport.csv' -NoTypeInformation
+		}
+
+
+# -----------------------------
+# Import Data
+# -----------------------------
+
+$ReportList = @()
+
+# -----------------------------
+# Main Processing
+# -----------------------------
+
+do{
+	'Please select from the following options:'
+
+	'1. Create User'
+	'2. Disable User'
+	'3. Unlock User'
+	'4. Add User to Group'
+	'5. Create a report'
+	'6. Exit'
+
+	$Choice = Read-Host "Enter your selection"
+
+	switch ($Choice) {
+		
+		"1"{
+			$Result = Create-User
+			$ReportList += $Result
+			}
+
+		"2" {
+			$Result = Disable-User
+			$ReportList += $Result
+			}
+			
+		"3" {
+			$Result = Unlock-User
+			$ReportList += $Result
+			}
+		"4" {
+			$Result = AddUserTo-Group
+			$ReportList += $Result
+			}
+		"5" {
+			Generate-Report
+			}
+		"6" {
+			'Exit'
+			}
+
+}
+
+
+		
+
+} until ($Choice -eq '6')
+
+Initial commit: Add core AD Management functions 
